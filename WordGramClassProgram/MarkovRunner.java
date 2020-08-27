2231f6d494b3269cc9ba773684c4acee3573cb7b
dragonlais <rapolasrusakevicius@gmail.com>
2020-08-28 00:48:03 +0300

    Add files via upload

---
+++ WordGramClassProgram/MarkovRunner.java
@@ -0,0 +1,49 @@
+import edu.duke.*;
+
+public class MarkovRunner {
+    public void runModel(IMarkovModel markov, String text, int size){ 
+        markov.setTraining(text); 
+        System.out.println("running with " + markov); 
+        for(int k=0; k < 3; k++){ 
+            String st = markov.getRandomText(size); 
+            printOut(st); 
+        } 
+    } 
+
+    public void runModel(IMarkovModel markov, String text, int size, int seed){ 
+        markov.setTraining(text); 
+        markov.setRandom(seed);
+        System.out.println("running with " + markov); 
+        for(int k=0; k < 3; k++){ 
+            String st = markov.getRandomText(size); 
+            printOut(st); 
+        } 
+    } 
+
+    public void runMarkov() {
+        FileResource fr = new FileResource(); 
+        String st = fr.asString(); 
+        st = st.replace('\n', ' ');
+        
+        MarkovWord markovWord = new MarkovWord(5);
+        runModel(markovWord, st, 120, 844);
+        
+        EfficientMarkovWord efficientMarkovWord = new EfficientMarkovWord(2);
+        runModel(efficientMarkovWord, st, 120, 65);
+    } 
+
+    private void printOut(String s){
+        String[] words = s.split("\\s+");
+        int psize = 0;
+        System.out.println("----------------------------------");
+        for(int k=0; k < words.length; k++){
+            System.out.print(words[k]+ " ");
+            psize += words[k].length() + 1;
+            if (psize > 60) {
+                System.out.println(); 
+                psize = 0;
+            } 
+        } 
+        System.out.println("\n----------------------------------");
+    } 
+}
